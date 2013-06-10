b311951bdd90802096d22269dfa2c9943bdbf3a6
Sammaye <sam.millman@googlemail.com>
2013-06-10 16:35:29 +0100

    LOL

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -8,48 +8,47 @@ use glue,
 class Menu extends \glue\Widget{
 
 	function render(){ ?>
-			<div class='menu' style=''>
-				<div class="menu_left">
-					<ul>
-						<li class='logo'><a href='<?php echo glue::http()->createUrl('/') ?>'><img src='/images/main_logo.png' alt='StageX'/></a></li>
-						<li class="search">
-							<form action="/search" method="get"><div class="search_input">
-								<?php
-									$val = '';
-									if(preg_match('/\/search/i', $_SERVER['REQUEST_URI'])){
-										$val = glue::http()->param('term', '');
-									}
+		<div class='menu' style=''>
+			<div class="menu_left">
+				<ul>
+					<li class='logo'><a href='<?php echo glue::http()->createUrl('/') ?>'><img src='/images/main_logo.png' alt='StageX'/></a></li>
+					<li class="search">
+						<form action="/search" method="get"><div class="search_input">
+							<?php
+								$val = '';
+								if(preg_match('/\/search/i', $_SERVER['REQUEST_URI'])){
+									$val = glue::http()->param('term', '');
+								}
 
-									glue::$controller->js('submitsearch_click', "
-										$(function(){
-											$('.submit_search').click(function(event){
-												event.preventDefault();
-												$(this).parents('form').submit();
-											});
+								glue::$controller->js('submitsearch_click', "
+									$(function(){
+										$('.submit_search').click(function(event){
+											event.preventDefault();
+											$(this).parents('form').submit();
 										});
-									");
+									});
+								");
 
-									\app\widgets\Jqautocomplete::widget(array(
-										'attribute' => 'term',
-										'value' => $val,
-										'options' => array(
-											'appendTo' => '#mainSearch_results',
-											'source' => '/search/suggestions',
-											'minLength' => 2,
-										),
-										'renderItem' => "
-											return $( '<li></li>' )
-												.data( 'item.autocomplete', item )
-												.append( '<a class=\'content\'>' + item.label + '</a>' )
-												.appendTo( ul );
-											"
-									)) ?></div><button class="submit_search"><span>&nbsp;</span></button>
-							</form>
-						</li>
-						<li class='link'><a href="<?php echo Glue::http()->createUrl("/video") ?>">Browse</a></li>
-					</ul>
-				</div>
-				<?php if(isset($_SESSION)){ ?>
+								\app\widgets\Jqautocomplete::widget(array(
+									'attribute' => 'term',
+									'value' => $val,
+									'options' => array(
+										'appendTo' => '#mainSearch_results',
+										'source' => '/search/suggestions',
+										'minLength' => 2,
+									),
+									'renderItem' => "
+										return $( '<li></li>' )
+											.data( 'item.autocomplete', item )
+											.append( '<a class=\'content\'>' + item.label + '</a>' )
+											.appendTo( ul );"
+								)) ?></div><button class="submit_search"><span>&nbsp;</span></button>
+						</form>
+					</li>
+					<li class='link'><a href="<?php echo Glue::http()->createUrl("/video") ?>">Browse</a></li>
+				</ul>
+			</div>
+			<?php if(isset($_SESSION)){ ?>
 				<div class="menu_right">
 					<ul>
 						<?php if(glue::session()->authed){ ?>
@@ -78,11 +77,11 @@ class Menu extends \glue\Widget{
 					</ul>
 				</div>
 				<div class="clearer"></div>
-				<?php } ?>
-			</div>
+			<?php } ?>
+		</div>
 
-			<?php if(html::hasFlashMessage()){
-				echo html::getFlashMessage();
-			}
+		<?php if(html::hasFlashMessage()){
+			echo html::getFlashMessage();
+		}
 	}
 }
