295f7f8ea1b4d74350fdcd40d55bfc4b6578fe4b
Sammaye <sam.millman@googlemail.com>
2013-12-30 10:45:29 +0000

    formatting

--- glue/Validator.php
+++ glue/Validator.php
@@ -2,16 +2,21 @@
 
 namespace glue;
 
+use glue\Component;
+
 /**
  * Extend this class to add your own validators
  */
-class Validator extends \glue\Component{
-	
+class Validator extends Component
+{
 	public $owner;
 	
-	function validateAttribute($model, $attribute, $value){}
+	function validateAttribute($model, $attribute, $value)
+	{
+	}
 
-	public function isEmpty($value, $trim  = false){
+	public function isEmpty($value, $trim  = false)
+	{
 		return $value===null || $value===array() || $value==='' || $trim && is_scalar($value) && trim($value)==='';
 	}
 }
