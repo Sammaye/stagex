0ca4e4a9a474994388a691c4117768243fb65585
Sammaye <sam.millman@googlemail.com>
2014-01-04 16:35:48 +0000

    Redone the cursor to implement query handling

--- glue/db/Cursor.php
+++ glue/db/Cursor.php
@@ -2,46 +2,32 @@
 
 namespace glue\db;
 
-use \glue\Exception;
+use Glue;
+use \Iterator;
+use \Countable;
+use glue\Component;
 
-class Cursor implements \Iterator, \Countable{
-
-	private $modelClass;
-	private $model;
-
-	private $cursor = array();
-	private $current;
-
-	/**
-	 * This denotes a partial cursor which in turn will transpose onto the active record
-	 * to state a partial document. If any projection is supplied this will result in true since
-	 * I cannot detect if you are projecting the whole document or not...THERE IS NO PRE-DEFINED SCHEMA
-	 * @var boolean
-	 */
-	private $partial = false;
-
-	/**
-	 * The cursor constructor
-	 * @param array|MongoCursor $condition Either a condition array (without sort,limit and skip) or a MongoCursor Object
-	 * @param string $class the class name for the active record
-	 */
-	public function __construct($modelClass,$cursor,$partial=false) {
-
-		// If $fields has something in it
-		if($partial)
-			$this->partial=true;
-
-	    if(is_string($modelClass)){
-			$this->modelClass=$modelClass;
-			$this->model=EMongoDocument::model($this->modelClass);
-		}elseif($modelClass instanceof \glue\db\Document){
-			$this->modelClass=get_class($modelClass);
-			$this->model=$modelClass;
-		}
-
-		$this->cursor=$cursor;
-		return $this; // Maintain chainability
-	}
+class Cursor extends Component implements Iterator, Countable
+{
+	public $select;
+	
+	public $model;
+	
+	public $from;
+	
+	public $with = array();
+	
+	public $where;
+	
+	public $sort;
+	
+	public $skip;
+	
+	public $limit;
+	
+	private $_mongoCursor;
+	
+	private $_run = false;
 
 	/**
 	 * If we call a function that is not implemented here we try and pass the method onto
@@ -54,71 +40,131 @@ class Cursor implements \Iterator, \Countable{
 		if($this->cursor() instanceof \MongoCursor && method_exists($this->cursor(), $method)){
 			return call_user_func_array(array($this->cursor(), $method), $params);
 		}
-		throw new Exception("Call to undefined function $method on the cursor");
+		
+		if($this->model !== null){
+			$class = $this->model;
+			if(method_exists($class, $method)){
+				array_unshift($params, $this);
+				call_user_func_array(array($class, $method), $params);
+			}
+		}
+		
+		parent::__call($method, $params);
 	}
 
 	/**
 	 * Holds the MongoCursor
 	 */
 	public function cursor(){
-		return $this->cursor;
+		return $this->_mongoCursor;
 	}
-
-    /**
-     * Get next doc in cursor
-     */
-    public function getNext(){
-		if($c=$this->cursor()->getNext())
-			return $this->current=$this->model->populateRecord($c,true,$this->partial);
-    }
+	
+	public function getCollection($db = null)
+	{
+		$modelClass = $this->model;
+		if($db === null && $modelClass !== null){
+			$db = $modelClass::getDb();
+		}elseif($db === null){
+			$db = glue::db();
+		}
+		
+		if($this->from === null){
+			$this->from = $modelClass::collectionName();
+		}
+		return $db->{$this->from};
+	}	
+	
+	public function one()
+	{
+		return $this->getCollection()->findOne($this->where, $this->select);
+	}
+	
+	public function all()
+	{
+		$cursor = $this->getCollection()
+			->find($this->where, $this->select)
+			->sort($this->sort)
+			->skip($this->skip)
+			->limit($this->limit);
+		$this->_mongoCursor = $cursor;
+		return $this;
+	}
+		
+	public function sort($fields)
+	{
+		$this->cursor()->sort($fields);
+		return $this;
+	}
+		
+	public function skip($num = 0)
+	{
+		$this->skip = $num;
+		return $this;
+	}
+		
+	public function limit($num = 0)
+	{
+		$this->limit = $num;
+		return $this;
+	}
+	
+	public function slaveOkay($val = true){
+		$this->cursor()->slaveOkay($val);
+		return $this;
+	}	
 
 	/**
 	 * Gets the active record for the current row
 	 */
-	public function current() {
-		if($this->model === null)
-			throw new Exception("The MongoCursor must have a model");
-		return $this->current=$this->model->populateRecord($this->cursor()->current(),true,$this->partial);
+	public function current($current = null)
+	{
+		if($current === null){
+			$current = $this->cursor()->current();
+		}
+		
+		if($this->model === null){
+			return $current;
+		}else{
+			$class = $this->model;
+			return $class::populate($this->cursor()->current(), true);
+		}
 	}
+	
+	public function getNext()
+	{
+		if($next = $this->cursor()->getNext()){
+			return $this->current($next);
+		}
+	}	
 
-	public function count($takeSkip = false /* Was true originally but it was to change the way the driver worked which seemed wrong */){
+	public function count($takeSkip = false)
+	{
 		return $this->cursor()->count($takeSkip);
 	}
-
-	public function slaveOkay($val = true){
-		$this->cursor()->slaveOkay($val);
-		return $this;
-	}
-
-	public function sort(array $fields){
-		$this->cursor()->sort($fields);
-		return $this;
-	}
-
-	public function skip($num = 0){
-		$this->cursor()->skip($num);
-		return $this;
-	}
-
-	public function limit($num = 0){
-		$this->cursor()->limit($num);
-		return $this;
-	}
-
-	public function rewind() {
+	
+	public function rewind()
+	{
+		if($this->_run === false){
+			$this->all();
+			$this->_run = true;
+		}		
+		
 		$this->cursor()->rewind();
 		return $this;
 	}
 
-	public function key() {
+	public function key()
+	{
 		return $this->cursor()->key();
 	}
 
-	public function next() {
+	public function next()
+	{
 		return $this->cursor()->next();
 	}
 
-	public function valid() {
+	public function valid()
+	{
 		return $this->cursor()->valid();
 	}
 }
