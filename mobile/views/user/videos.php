63aa5d6079b11d7ad63d4ea0169c462ceb4e8e12
Sammaye <sam.millman@googlemail.com>
2014-02-10 23:36:21 +0000

    crap

--- mobile/views/user/videos.php
+++ mobile/views/user/videos.php
@@ -123,15 +123,15 @@ $this->js('videos', "
 			</div><span class='text-muted small amount_found'><?php echo $video_rows->count() ?> found</span>
 			<div class="advnaced_search">
 				<div class="row condition_row">
-				<div class="form-group col-md-3 col-sm-3 col-xs-6 condition_field"><?php echo $form->selectbox('field[]', app\models\Video::advancedSearchFields(), null, array('class' => 'form-control')) ?></div>
-				<div class="form-group col-md-2 col-sm-2 col-xs-6 condition_type">
-				<div class="rangevalue-field"><?php echo $form->selectbox('search_op[]', array('=' => '=', '>' => '>', '>=' => '>=', '<' => '<', '<=' => '<=', '!=' => '!=', 
-					'in' => 'IN', 'not_in' => 'NOT IN', 'null' => 'Is Empty'), null, array('class' => 'form-control')) ?></div>
+				<div class="form-group col-md-2 col-sm-2 col-xs-6 condition_field"><?php echo $form->selectbox('field[]', app\models\Video::advancedSearchFields(), null, array('class' => 'form-control')) ?></div>
+				<div class="form-group col-md-2 col-sm-2 col-xs-6 condition_type" style='width:140px;'>
+				<?php echo $form->selectbox('search_op[]', array('=' => '=', '>' => '>', '>=' => '>=', '<' => '<', '<=' => '<=', '!=' => '!=', 
+					'in' => 'IN', 'not_in' => 'NOT IN', 'like' => 'LIKE', 'not_like' => 'NOT LIKE', 'null' => 'Is Empty'), null, array('class' => 'form-control')) ?>
 				</div>
-				<div class="form-group col-md-3 col-sm-3 col-xs-6 condition_value">
-				<div class="textvalue-value"><?php echo $form->textfield('vlaue[]', null, 'form-control') ?></div>
+				<div class="form-group col-md-5 col-sm-5 col-xs-7 condition_value">
+				<div class="textvalue-value"><?php echo $form->textfield('value[]', null, array('placeholder' => 'Enter Search Query', 'class' => 'form-control')) ?></div>
 				</div>
-				<div class="form-group col-md-2 col-sm-2 col-xs-4 condition_operand">
+				<div class="form-group col-md-2 col-sm-2 col-xs-3 condition_operand" style='width:140px;'>
 				<?php echo $form->selectbox('operand[]', array('and' => 'AND', 'or' => 'OR', 'and_or' => 'AND OR'), null, array('class' => 'form-control')) ?>
 				</div>
 				<div class="form-group col-md-1 col-sm-1 col-xs-2"><a class="condition_remove" href="#">Remove</a></div>
